974ea996ca1a4604aed6eae7af0f76fe94710d39
Jhon Quintero <jfqm7710@gmail.com>
2025-08-29 21:41:12 -0500

    refactor(comments): deleting comments

--- src/products/products.service.ts
+++ src/products/products.service.ts
@@ -1,13 +1,9 @@
 import { PrismaClient } from 'generated/prisma'; // import PrismaClient from @prisma/client
 import { CreateProductDto } from './dto/create-product.dto';
 import { UpdateProductDto } from './dto/update-product.dto';
-import {
-  Injectable,
-  Logger,
-  NotFoundException,
-  OnModuleInit,
-} from '@nestjs/common';
+import { HttpStatus, Injectable, Logger, OnModuleInit } from '@nestjs/common';
 import { PaginationDto } from 'src/common';
+import { RpcException } from '@nestjs/microservices';
 
 @Injectable()
 export class ProductsService extends PrismaClient implements OnModuleInit {
@@ -53,7 +49,10 @@ export class ProductsService extends PrismaClient implements OnModuleInit {
     });
 
     if (!data) {
-      throw new NotFoundException(`Product with id #${id} not found`);
+      throw new RpcException({
+        message: `Product with id #${id} not found`,
+        status: HttpStatus.BAD_REQUEST,
+      });
     }
 
     return { data };
@@ -71,8 +70,6 @@ export class ProductsService extends PrismaClient implements OnModuleInit {
 
   async remove(id: number) {
     await this.findOne(id);
-
-    // return await this.product.delete({ where: { id } });
 
     const product = await this.product.update({
       where: { id },
